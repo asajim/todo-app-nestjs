fde092a1c96d388198168ad9681bd3a06f78600f
Ade Sajim <ade.sajim@gmail.com>
2023-04-25 20:06:25 +0200

    Add test env

--- config/validation.ts
+++ config/validation.ts
@@ -1,7 +1,7 @@
 import * as Joi from 'joi';
 
 export const validationSchema = Joi.object({
-  NODE_ENV: Joi.string().valid('development', 'production'),
+  NODE_ENV: Joi.string().valid('development', 'production', 'test'),
   PORT: Joi.number().required(),
   AUTH_USERNAME: Joi.string().required(),
   AUTH_PASSWORD: Joi.string().required(),
